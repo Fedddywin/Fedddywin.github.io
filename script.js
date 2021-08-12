525dbb260d4b7bbc5e9d060f4d3372d691ec10f6
Fedddywin <defogle@g.clemson.edu>
2021-08-12 16:23:23 -0400

    Cookie Clicker update
    Uses both HTML and JavaScript

---
+++ script.js
@@ -0,0 +1,47 @@
+var cookieCount = 0;
+var cookie = document.getElementById("cookie");
+
+cookie.onclick = function() {
+    cookieCount++;
+    alert(cookieCount);
+    
+}
+var countElement = document.getElementById("count");
+    cookie.onclick = function() {
+        cookieCount++
+        countElement.innerHTML = cookieCount;
+}
+
+var cps = 0;
+var cursorCount = 0;
+var cursorCost = 0;
+
+document.getElementById("buyCursorBtn").onclick = function() {
+	
+}
+
+document.getElementById("buyCursorBtn").onclick = function() {
+	if (cookieCount >= cursorCost) {
+		cookieCount -= cursorCost;
+		cursorCount++;
+		cps++;
+	}
+}
+
+setInterval(function() {
+	cookieCount += cps;
+},1000);
+
+
+cursorCountElement = document.getElementById("cursorCount");
+setInterval(function() {
+	cookieCount += cps;
+	countElement.innerHTML = cookieCount;
+},1000);
+document.getElementById("buyCursorBtn").onclick = function() {
+	cookieCount -= cursorCost;
+	countElement.innerHTML = cookieCount;
+	cursorCount++;
+	cursorCountElement.innerHTML = cursorCount;
+	cps++;
+}
