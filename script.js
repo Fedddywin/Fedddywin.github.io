4be825437014acbf6b530b93c7129cb2b34de1c9
Fedddywin <defogle@g.clemson.edu>
2021-08-15 23:14:21 -0400

    Storage!

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
+let x
 
-let x = 0
+
+if (localStorage.getItem("count"))
+{
+	x = parseInt(localStorage.getItem("count"))
+}
+else 
+{
+	x = 0 
+}
+
 document.getElementById("counter").innerHTML = x
 document.getElementById("btn").addEventListener("click", iterate)
 
 function iterate(){
 	x += 1
+	localStorage.setItem("count", x)
 	console.log(x)
 	document.getElementById("counter").innerHTML = x
 }
