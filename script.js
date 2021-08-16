1500387f32052f483ef9cdea78b7af4c1c170d89
Fedddywin <defogle@g.clemson.edu>
2021-08-15 23:03:39 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 
-const some_action = () => {
-	console.log("clicked")
+let x = 0
+document.getElementById("counter").innerHTML = x
+document.getElementById("btn").addEventListener("click", iterate)
+
+function iterate(){
+	x += 1
+	console.log(x)
+	document.getElementById("counter").innerHTML = x
 }
-document.getElementById("btn").addEventListener("click",some_action)
