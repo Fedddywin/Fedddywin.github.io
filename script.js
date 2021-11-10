89f86c34f8ff8f4fee9d04865b915f240431eeee
Fedddywin <defogle@g.clemson.edu>
2021-11-09 23:57:08 -0500

    Reintroduction  of Clicker Website
    Brought back Clicker program for further experimentation.
    Also added small comments to help break down javaScript Code.

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
+let x
 
+//Storage Check
+if (localStorage.getItem("count"))
+{
+	x = parseInt(localStorage.getItem("count"))
+}
+else 
+{
+	x = 0 
+}
+
+//Allows access to counter
+document.getElementById("counter").innerHTML = x
+
+//Allows access to 1st Iterate event
+document.getElementById("btn").addEventListener("click", iterate)
+
+//Once 25 clicks are reached, allows access to 2nd Iterate Event
+while (x >= 25)
+{
+document.getElementById("btn2").addEventListener("click",iterate2)
+}
+
+//1st iterate event
+function iterate(){
+	x += 1
+	localStorage.setItem("count", x)
+	console.log(x)
+	document.getElementById("counter").innerHTML = x
+}
+
+//2nd Iterate event
+function iterate2(){
+	x += 2
+	localStorage.setItem("count", x)
+	console.log(x)
+	document.getElementById("counter").innerHTML = x
+}
